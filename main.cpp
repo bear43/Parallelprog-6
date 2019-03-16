6246070c42a1bb16b15c420e4669d7dadc90afc6
bear43 <bigwilly4307@gmail.com>
2019-03-17 02:30:12 +0400

    Second version.

--- main.cpp
+++ main.cpp
@@ -2,18 +2,20 @@
 #include <stdio.h>
 #include <memory.h>
 #define NUM_DIMS 1
+#define BUFFER_SIZE 64
+#define BUFFER_VALUE "DJ-eban"
 
 int main(int argc, char **argv)
 {
-    int rank, size, A,B, dims[NUM_DIMS];
+    int rank, size, dims[NUM_DIMS];
     int periods[NUM_DIMS],source, dest;
     int reorder=0;
+    char buffer[BUFFER_SIZE];
     MPI_Comm comm_cart;
     MPI_Status status;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    A=rank;	B=-1;
     /* Обнуляем массив dims и заполняем массив periods для топологии "кольцо" */
     memset(dims, 0, sizeof(dims));
     memset(periods, true, sizeof(dims));
@@ -29,26 +31,21 @@ int main(int argc, char **argv)
     данные от ветви size-1. */
     if(rank == 0)
     {
-        char *buf = new char();
-        MPI_Send(buf, 1, MPI_CHAR, rank+1, 0, comm_cart);
-        MPI_Sendrecv(&A, 1, MPI_INT, dest, 12, &B, 1, MPI_INT, source, 12, comm_cart, &status);
-        MPI_Recv(buf, 1, MPI_CHAR, size-1, 0, comm_cart, MPI_STATUS_IGNORE);
-        delete buf;
+        strcpy(buffer, BUFFER_VALUE);
+        MPI_Send(buffer, BUFFER_SIZE, MPI_CHAR, dest, 0, comm_cart);
+        MPI_Recv(buffer, BUFFER_SIZE, MPI_CHAR, size-1, 0, comm_cart, MPI_STATUS_IGNORE);
     }
     else
     {
-        char *buf = new char();
-        MPI_Recv(buf, 1, MPI_CHAR, rank-1, 0, comm_cart, MPI_STATUS_IGNORE);
-        MPI_Sendrecv(&A, 1, MPI_INT, dest, 12, &B, 1, MPI_INT, source, 12, comm_cart, &status);
+        MPI_Recv(buffer, BUFFER_SIZE, MPI_CHAR, rank-1, 0, comm_cart, MPI_STATUS_IGNORE);
         if(rank < size-1)
-            MPI_Send(buf, 1, MPI_CHAR, rank+1, 0, comm_cart);
+            MPI_Send(buffer, BUFFER_SIZE, MPI_CHAR, dest, 0, comm_cart);
         else
-            MPI_Send(buf, 1, MPI_CHAR, 0, 0, comm_cart);
-        delete buf;
+            MPI_Send(buffer, BUFFER_SIZE, MPI_CHAR, 0, 0, comm_cart);
     }
     /* Каждая ветвь печатает свой ранг (он же и был послан соседней ветви с большим рангом и значение
     переменной B (ранг соседней ветви с меньшим рангом). */
-    printf("rank=%d B=%d\n", rank, B);
+    printf("rank=%d Buffer=%s\n", rank, buffer);
     fflush(stdout);
     /* Все ветви завершают системные процессы, связанные с топологией comm_cart и завершают
     выполнение программы */
